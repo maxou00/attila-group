acffbc01429b7d3ccb0ba2fd97a1c8605671cd7d
themx05 <maximiliencomlan05@gmail.com>
2021-09-07 12:04:00 +0100

    added responsiveness

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,20 @@ import Splide from '@splidejs/splide';
 import '@splidejs/splide/dist/css/splide.min.css';
 import './style.scss';
 
-declare global  {
+declare global {
     interface Window {
         initMap(): any;
     }
 }
 
 
-let serviceSplide = new Splide(".splide",{
+let serviceSplide = new Splide(".splide", {
     type: "loop",
     autoplay: true,
     perPage: 3,
     perMove: 1,
     breakpoints: {
-        840:{
+        840: {
             perPage: 2
         },
         560: {
@@ -26,12 +26,12 @@ let serviceSplide = new Splide(".splide",{
 
 
 function init() {
-    const pos = {lat: -25.344, lng: 131.036};
+    const pos = { lat: -25.344, lng: 131.036 };
     const map = new google.maps.Map(
         document.querySelector(".map-area") as HTMLElement, {
-            zoom: 8,
-            center: pos
-        }
+        zoom: 8,
+        center: pos
+    }
     )
 
     new google.maps.Marker({
@@ -41,10 +41,24 @@ function init() {
 
 }
 
+function setupMobile() {
+    let mobileBurger = document.querySelector(".menu-open") as HTMLElement;
+    let menuCloser = document.querySelector(".btn-close") as HTMLElement;
+    let mobileMenu = document.querySelector(".mobile-header") as HTMLElement;
+
+    menuCloser.onclick = (ev) => {
+        mobileMenu.setAttribute("data-open", "false");
+    }
+
+    mobileBurger.onclick= (ev) => {
+        mobileMenu.setAttribute("data-open", "true");
+    }
+}
+
 window.initMap = init;
 
 document.addEventListener("DOMContentLoaded", () => {
+    setupMobile();
     serviceSplide.mount();
     init();
 })
-
